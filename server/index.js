86cb4d410d868b598a4a61d3ad47c26ddb988c88
Sam Couch <samcouch91@gmail.com>
2015-10-08 20:53:24 -0400

    start server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var path = require("path");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(3000, () => {
+  console.log('All good on 3000');
+})
